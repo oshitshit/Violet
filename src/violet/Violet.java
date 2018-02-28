3eb46887937ac17a87cc7ee75a9b6fb635ae6a1f
oshitshit <15314128337@163.com>
2018-02-28 09:43:24 +0800

    change

--- src/violet/Violet.java
+++ src/violet/Violet.java
@@ -3,6 +3,6 @@ package violet;
 public class Violet {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("nice");
+		System.out.println("yes yes yes ");
 	}
 }
